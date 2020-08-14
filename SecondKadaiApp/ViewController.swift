602d9be84f923e7ad404c81075b1a4c6c3420a37
dan.inano <dan.inano@icloud.com>
2020-08-14 23:51:53 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Dan Inano on 2020/08/14.
+//  Copyright © 2020 Dan Inano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
